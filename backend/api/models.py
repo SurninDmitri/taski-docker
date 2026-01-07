aee0ff1bbb2506f8df7a2c3a4bcf32bcf0f9ee4c
Дмитрий Сурнин <dim.surn1n@yandex.ru>
2026-01-07 11:23:57 +0300

    Add tests

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,6 +5,6 @@ class Task(models.Model):
     title = models.CharField(verbose_name='Заголовок', max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
-
+    
     def _str_(self):
         return self.title
